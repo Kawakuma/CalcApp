cd454eaad25817030bb0897d61e45108811d55ce
Kawakuma <kawakuma.1212854@outlook.jp>
2020-12-03 22:30:27 +0900

    コミット１

---
+++ app/src/main/java/jp/techacademy/kentaro/kawamura/calcapp/MainActivity.kt
@@ -0,0 +1,97 @@
+package jp.techacademy.kentaro.kawamura.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        plus.setOnClickListener(this)
+        minus.setOnClickListener(this)
+        multi.setOnClickListener(this)
+        divide.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+
+        if (editText1.text.toString() != "" && editText2.text.toString() != "") {
+
+            val intent = Intent(this, SecondActivity::class.java)
+            var Tx1St = editText1.text.toString()
+            var Tx1int = Tx1St.toDouble()
+            var Tx2St = editText2.text.toString()
+            var Tx2int = Tx2St.toDouble()
+
+            if (v.id == R.id.plus) {
+
+                var Sum = Tx1int + Tx2int
+                intent.putExtra("Value", Sum)
+                startActivity(intent)
+            }
+
+            else if (v.id==R.id.minus){
+
+                var Sum = Tx1int - Tx2int
+                intent.putExtra("Value", Sum)
+                startActivity(intent)
+
+            }
+
+        }
+            else {
+                Snackbar.make(v, "数値を入力してください", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Action"){
+                        Log.d("UI_PARTS", "Snackbarをタップした")
+                    }.show()
+            }
+
+
+
+
+
+        if (editText1.text.toString() != "" && editText2.text.toString() != "") {
+            if (v.id == R.id.multi) {
+                val intent = Intent(this, SecondActivity::class.java)
+                var Tx1St = editText1.text.toString()
+                var Tx1int = Tx1St.toDouble()
+                var Tx2St = editText2.text.toString()
+                var Tx2int = Tx2St.toDouble()
+                var Sum = Tx1int * Tx2int
+                intent.putExtra("Value", Sum)
+                startActivity(intent)
+            } else {
+                Snackbar.make(v, "数値を入力してください", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Action"){
+                        Log.d("UI_PARTS", "Snackbarをタップした")
+                    }.show()
+            }
+        }
+        if (editText1.text.toString() != "" && editText2.text.toString() != "") {
+            if (v.id == R.id.divide) {
+                val intent = Intent(this, SecondActivity::class.java)
+                var Tx1St = editText1.text.toString()
+                var Tx1int = Tx1St.toDouble()
+                var Tx2St = editText2.text.toString()
+                var Tx2int = Tx2St.toDouble()
+                var Sum = Tx1int / Tx2int
+                intent.putExtra("Value", Sum)
+                startActivity(intent)
+            } else {
+                Snackbar.make(v, "数値を入力してください", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Action"){
+                        Log.d("UI_PARTS", "Snackbarをタップした")
+                    }.show()
+            }
+        }
+    }
+}
+
+
